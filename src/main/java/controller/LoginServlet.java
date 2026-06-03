7f2fd8ff7265d064b429b64f168f23f69ae8bc19
Gaetano <gaetanofrati05@gmail.com>
2026-06-03 20:54:58 +0200

    Refactor LoginServlet for better readability

--- src/main/java/controller/LoginServlet.java
+++ src/main/java/controller/LoginServlet.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
