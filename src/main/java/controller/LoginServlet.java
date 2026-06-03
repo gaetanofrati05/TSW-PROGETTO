b0e9ddfffa9a3cb4ac6f1856c3920a12ecdbacc0
Gaetano <gaetanofrati05@gmail.com>
2026-06-03 20:46:05 +0200

    Implement login functionality in LoginServlet

--- src/main/java/controller/LoginServlet.java
+++ src/main/java/controller/LoginServlet.java
@@ -1,11 +1,15 @@
 package control;
-
+import dao.UtenteDAO;
+import utils.PasswordEncryption;
+import bean.UtenteBean;
 import java.io.IOException;
+import java.sql.SQLException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LoginServlet
@@ -13,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    private UtenteDAO utenteDAO;
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -23,19 +27,31 @@ public class LoginServlet extends HttpServlet {
     }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String email =request.getParameter("email");
+		String password=request.getParameter("password");
+		String passwordCifrata= PasswordEncryption.encrypt(password);
+		try {
+			UtenteBean utente= utenteDAO.doRetrieveByEmailAndPassword(email, passwordCifrata);
+			if(utente!=null) {
+				HttpSession session = request.getSession(true);
+				session.setAttribute("utenteLoggato", utente);
+				//controllo se l'utente è admin oppure no
+				if(utente.getAdmin()) {
+					System.out.println("Benvenuto admin" + utente.getNome());
+					response.sendRedirect(request.getContextPath() +"/jsp/admin.jsp");				
+				}else {
+					System.out.println("Benvenuto"+ utente.getNome());
+					response.sendRedirect(request.getContextPath()+ "/jsp/index.jsp");
+				}
+			}else {
+				request.setAttribute("erroredilogin", "Email o password errate");
+				request.getRequestDispatcher("/jsp/login.jsp").forward(request, response);
+			}			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
